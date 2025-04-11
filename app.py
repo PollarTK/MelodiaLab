c3a7747b85e2bce7203287f59cca50699afd9e08
pollartk <felipebeckmann18@gmail.com>
2025-04-11 17:09:28 -0300

    cadastro, login, excluir

--- app.py
+++ app.py
@@ -1,45 +1,75 @@
-from flask import Flask, render_template, request, url_for, redirect, flash
+from flask import Flask, render_template, request, url_for, redirect, flash, session
 app = Flask(__name__)
 app.secret_key = "chave_muito_segura"
-
-# Cria um dicionário e usuários e senha, SERÁ MIGRADO PARA O BANCO DE DADOS
-usuarios = {
-    'usuario' : 'senha',
-    'admin' : 'admin'
-}
+import database
 
 @app.route('/') #rota para a página inicial
 def index():
     return render_template('index.html')
 
-@app.route('/login') #rota para a página de login
+@app.route('/login', methods=["GET", "POST"]) #rota para a página de login
 def login():
-    return render_template('login.html')
+    if request.method == "POST":
+        form = request.form
+        if database.fazer_login(form) == True:
+            session['usuario'] = form['email'] # Armazena o email do usuário na sessão
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html')
+    else:
+        return render_template('login.html')
 
 @app.route('/home')
 def home():
     return render_template('home.html')
 
+@app.route('/cadastro', methods=["GET","POST"]) #rota para a página de login
+def cadastro():
+    if request.method == "POST":
+        form = request.form
 
-# VERFIFICAR O LOGIN
-@app.route('/verificar-login', methods=['POST'])
-def verificar_login():
-# Pegando o que o usuário digitou no campo de entrada de user e senha
-    username = request.form['username']
-    password = request.form['password']
+        if database.criar_usuario(form) == True:
+            return render_template('login.html')
+        
+        else:
+            return "Ocorreu um erro ao cadastrar o Usuário"
 
-    # Verifica se o usuario digitado está na lista e se 
-    # a senha está certa
-    if username in usuarios and usuarios[username] == password:
+    else:
+        return render_template('cadastro.html')
+    
+@app.route('/criar_musica', methods=["GET","POST"]) #rota para a pagina de criação
+def criar_musica():
+    if request.method == "POST":
+        form = request.form
+
+        if database.criar_musica(form['titulo'],form['conteudo'],form['status'], session['usuario']) == True:
+            return redirect(url_for('home'))
+        
+        else:
+            pass
+
+    else:
+        return render_template('criar_musica.html')
+    
+@app.route('/excluir_usuario')
+def excluir_usuario():
+    email = session['usuario']
+
+    if database.excluir_usuario(email):
+        return redirect(url_for('cadastro'))
+    else:
+        return "Ocorreu um erro ao excluir o usuário"
+    
+@app.route('/musicas/excluir/<int:id>', methods=["GET"])
+def excluir_musica(id):
+
+    email = session['usuario']
+
+    if database.excluir_musica(id, email):
         return redirect(url_for('home'))
     else:
-        # Flash envia mensagem para o front-end
-        flash('Usuário ou senha incorretos', 'danger')
-        return redirect(url_for('login'))
+        return "Ocorreu um erro ao excluir a Música!"
 
-@app.route('/cadastro') #rota para a página de login
-def cadastro():
-    return render_template('cadastro.html')
 
 # parte principal do
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,5 +1,93 @@
 import sqlite3
+from werkzeug.security import generate_password_hash,check_password_hash
+
+def conectar_banco():
+    conexao = sqlite3.connect("musicas.db")
+    return conexao
+
+def criar_tabelas():
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''create table if not exists usuarios
+                   (email text primary key, nome text, senha text)''' )
+    
+    cursor.execute('''create table if not exists musicas (id integer primary key, titulo text, conteudo text, status text, email_usuario text,
+             FOREIGN KEY(email_usuario) REFERENCES usuarios(email))''')
+
+    conexao.commit()
+
+def criar_usuario(formulario):
+    # Verifica se ja existe esse email no banco de dados
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''SELECT count(email) from usuarios WHERE email=?''',(formulario['email'],))
+    conexao.commit()
+
+    quantidade_de_emails = cursor.fetchone()
+    if(quantidade_de_emails[0] > 0):
+        print("LOG: Já existe esse email cadastrado no banco!")
+        return False
+    
+    senha_criptografada = generate_password_hash(formulario['senha'])
+    cursor.execute('''INSERT INTO usuarios (email, nome, senha)
+                        VALUES (?, ?, ?)''', (formulario['email'], formulario['nome'], senha_criptografada))
+    conexao.commit()
+    return True
+
+def fazer_login(formulario):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''SELECT count(email) from usuarios WHERE email=?''',(formulario['email'],))
+    conexao.commit()
+
+    quantidade_de_emails = cursor.fetchone()
+    if(quantidade_de_emails[0] < 0):
+        print("LOG: email não encontrado.")
+        return False
+    else:
+        try: 
+            cursor = conexao.cursor()
+            cursor.execute('''SELECT (senha) from usuarios WHERE email=?''',(formulario['email'],))
+            conexao.commit()
+            senha_criptografada = cursor.fetchone()
+            resultado_verificacao = check_password_hash(senha_criptografada[0], formulario['senha'])
+            return resultado_verificacao
+        except:
+            return False
+        
+def criar_musica(titulo,conteudo,status,email):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute(''' INSERT INTO musicas (titulo, conteudo, status, email_usuario)
+                   VALUES (?,?,?,?)''', 
+                   (titulo,conteudo, status, False, email))
+    conexao.commit()
+    return True
+
+def excluir_musica(id, email):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''SELECT email_usuario FROM musicas WHERE id=?''', (id,))
+    conexao.commit()
+    email_banco = cursor.fetchone()
+    print(email, email_banco)
+    if (email_banco[0] !=email):
+        
+        return False
+    else:
+        cursor.execute(''' DELETE FROM musicas WHERE id=?''', (id,))
+        conexao.commit()
+        cursor.close()
+        return True
+
+def excluir_usuario(email):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('DELETE FROM musicas WHERE email_usuario=?',(email,))
+    cursor.execute('DELETE FROM usuarios WHERE email=?',(email,))
+    conexao.commit()
+    return True
 
 # PARTE PRINCIPAL DO PROGRAMA
 if __name__ == '__main__':
-    print("Hello, world!")
+    criar_tabelas()
